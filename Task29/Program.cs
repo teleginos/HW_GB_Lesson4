0c665abeea6174dbd095ef429b9ef69baa813831
Oleg Telegin <teleginos@mail.ru>
2023-05-20 13:30:41 +0300

    add solution task29

---
+++ Task29/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] ArrayGenerator()
+{
+    int[] Array = new int[8];
+    for(int i = 0; i < 8; i++)
+    {
+        Array[i] = new Random().Next(1,1000);
+    }
+    return Array;
+}
+
+Console.WriteLine($"[{string.Join(",", ArrayGenerator())}]");
