251dc9a86fe9ebd12351e6fa127d738fe70ed279
Oleg Telegin <teleginos@mail.ru>
2023-05-20 12:41:09 +0300

    add solution task25

---
+++ Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿double NewPow(int a, int b)
+{
+    return Math.Pow(a, b);
+}
+
+string CheckingTheInput(string number, string degree)
+{
+    if(int.TryParse(number, out var newNumber) && int.TryParse(degree, out var newDegree))
+    {
+        double result = NewPow(newNumber, newDegree);
+        return $"Результат возведения числа {number} в степень {degree} = {result}";
+    }
+
+    else
+    {
+        return "Введены не коректные данные";
+    }
+}
+
+Console.WriteLine("Для остановки программы введите 0");
+
+while(true)
+{
+    
+    Console.Write("Введите первое число: ");
+    string num1 = Console.ReadLine();
+    if(num1 == "0") break;
+    Console.Write("Введите второе число: ");
+    string num2 = Console.ReadLine();
+    Console.WriteLine($"{CheckingTheInput(num1, num2)}\n");
+    
+}
