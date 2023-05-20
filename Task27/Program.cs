09c5114911f061075782e6a57b9c360557dddc9d
Oleg Telegin <teleginos@mail.ru>
2023-05-20 13:18:46 +0300

    add solution task27

---
+++ Task27/Program.cs
@@ -0,0 +1,29 @@
+﻿int SumNumber(int number)
+{
+
+    int sum = 0;
+    while(number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+
+string CheckingTheInput(string num)
+{
+    if(int.TryParse(num, out var newNum))
+    {
+        int result = SumNumber(Convert.ToInt32(newNum));
+        return $"Суммы цифр числа {newNum} = {result}";
+    }
+
+    else
+    {
+        return "Введены не коректные данные";
+    }
+}
+
+Console.Write("Введите число: ");
+Console.WriteLine(CheckingTheInput(Console.ReadLine()));
